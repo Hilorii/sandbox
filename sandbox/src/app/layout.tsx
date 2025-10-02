0e1bc152cd604ed50006940e1be070232277b1ec
hilori <hilori.furan@wp.pl>
2025-10-02 23:16:38 +0200

    Tailwind changes + some visuals

--- sandbox/src/app/layout.tsx
+++ sandbox/src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className="m-0">
+      <body className="flex min-h-screen flex-col">
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
